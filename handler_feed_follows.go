c1a2ab70119fdea14fcfab22a112bfdd70331c3c
Malith Kuruppu <info.mkappworks@gmail.com>
2024-04-14 20:54:28 +0200

    Added handlerDeleteFeedFollow

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
 	"github.com/mkappworks/rss-aggregator-gobackend/internal/database"
 )
@@ -46,5 +47,27 @@ func (apiCfg *apiConfig) handlerGetFeedFollows(w http.ResponseWriter, r *http.Re
 		return
 	}
 
-	respondWithJSON(w, http.StatusOK, databaseFeedFollowstoFeedFollows(feedFollows))	
+	respondWithJSON(w, http.StatusOK, databaseFeedFollowstoFeedFollows(feedFollows))
+}
+
+func (apiCfg *apiConfig) handlerDeleteFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
+	feedFollowIdStr := chi.URLParam(r, "feedFollowID")
+	feedFollowId, err := uuid.Parse(feedFollowIdStr)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, fmt.Sprintf("Error parsing feed follow ID %v", err))
+		return
+	}
+
+	err = apiCfg.DB.DeleteFeedFollow(r.Context(), database.DeleteFeedFollowParams{
+		ID:     feedFollowId,
+		UserID: user.ID,
+	})
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error deleting feed follow %v", err))
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, nil)
 }
